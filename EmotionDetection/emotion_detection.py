cdee91bb9e7205a46514b1bf7fc244514ce6db18
AnantMoorthy <moorthyanant@gmail.com>
2024-05-14 02:05:02 -0400

    changes made from theia lab environment"

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,21 +7,33 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    emotion = formatted_response['emotionPredictions'][0]['emotion']
-    dom_emotion = max(emotion, key=emotion.get)
-    anger = emotion['anger']
-    disgust = emotion['disgust']
-    fear = emotion['fear']
-    joy = emotion['joy']
-    sadness = emotion['sadness']
-    output_format = {
-                    'anger': anger,
-                    'disgust': disgust,
-                    'fear': fear,
-                    'joy': joy,
-                    'sadness': sadness,
-                    'dominant_emotion': dom_emotion
-                    }
-    output_format["dominant_emotion"] = max(emotion, key=emotion.get)
+    if response.status_code == 400:
+        output_format = {'anger': None,
+                            'disgust': None, 
+                            'fear': None, 
+                            'joy': None, 
+                            'sadness': None, 
+                            'dominant_emotion': None
+                        }
+        return output_format
+
+    elif response.status_code == 200:
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
+        dom_emotion = max(emotion, key=emotion.get)
+        anger = emotion['anger']
+        disgust = emotion['disgust']
+        fear = emotion['fear']
+        joy = emotion['joy']
+        sadness = emotion['sadness']
+        output_format = {
+                        'anger': anger,
+                        'disgust': disgust,
+                        'fear': fear,
+                        'joy': joy,
+                        'sadness': sadness,
+                        'dominant_emotion': dom_emotion
+                        }
+        output_format["dominant_emotion"] = max(emotion, key=emotion.get)
     return output_format
+   
     #return response.text
--- server.py
+++ server.py
@@ -1,14 +1,20 @@
+''' Executing this function initiates the application of emotion detection to be
+    executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
+'''this is to avoid error'''
 @app.route("/emotionDetector")
 def sent_detector():
+    '''function for receiving text and running through emotion detector'''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     if response["dominant_emotion"] is None:
-        return "Invalid input ! Try again."
+        return "Invalid text! Please try again!"
     if response["dominant_emotion"] is not None:
         return (f"For the given statement, the system response is 'anger': {response['anger']},"
     f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and "
@@ -22,4 +28,4 @@ def render_index_page():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
